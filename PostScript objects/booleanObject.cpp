0f5aab9c5e6960bf3579922fa3b4f6e2dc0d84b9
Nate <nate@useclark.com>
2017-10-18 11:51:57 -0400

    10-18-2017

--- PostScript objects/booleanObject.cpp
+++ PostScript objects/booleanObject.cpp
@@ -1,10 +1,13 @@
+// Copyright 2017 InnoVisioNate Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "PostScript objects\booleanObject.h"
 
    booleanObject::booleanObject(char *psz) :
       object(psz,object::logical)
    {
-   if ( 0 == stricmp(psz,"true") )
+   if ( 0 == _stricmp(psz,"true") )
       value = true;
    else
       value = false;
--- include/PostScript.h
+++ include/PostScript.h
@@ -1,8 +1,8 @@
+// Copyright 2017 InnoVisioNate Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #pragma once
-
-#pragma warning(disable:4355)
-#pragma warning(disable:4996)
 
 #include <windows.h>
 #include <olectl.h>
