eebc338cd4216f0d7ee45b644e4360dd107971ff
ntclark <innovisionate@gmail.com>
2022-11-05 09:40:58 -0400

    Moved to PostScript sub-directory

--- include/resourceIdentifiers.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 InnoVisioNate Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#define ID_GLYPH_LIST    1
-
-#define WM_FLUSH_LOG       (WM_USER + 1)
