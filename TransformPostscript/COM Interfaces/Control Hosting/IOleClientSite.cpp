9b8cc85d4d729f30f86e45c2ef5f79f0aadb2f28
unknown <innovisionate@gmail.com>
2024-05-08 16:01:46 -0400

    asdf

--- TransformPostscript/COM Interfaces/Control Hosting/IOleClientSite.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2017, 2018, 2019 InnoVisioNate Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "ParsePostscript.h"
-
-#include "interfacesToSupportAnEmbeddedObject_IOleClientSite.cpp"
